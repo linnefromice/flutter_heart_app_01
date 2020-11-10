2f910680e32ad9b97ab07d4f0c7b8a8c3774a09b
linnefromice <linnefromice@gmail.com>
2020-11-11 01:20:07 +0900

    update gesture_detector_screen - add blocks

--- lib/screens/gesture_detector_screen.dart
+++ lib/screens/gesture_detector_screen.dart
@@ -19,35 +19,53 @@ class _GestureDetectorScreenState extends State<GestureDetectorScreen> {
       appBar: AppBar(
         title: Text('GestureDetector'),
       ),
-      body: GestureDetector(
-        onVerticalDragStart: (details) => setState(() => _messageOnVerticalDragStart = "globalPosition:${details.globalPosition}" ),
-        onVerticalDragUpdate: (details) => setState(() => _messageOnVerticalDragUpdate = "globalPosition:${details.globalPosition}\ndelta:${details.delta}" ),
-        onVerticalDragEnd: (details) => setState(() => _messageOnVerticalDragEnd = "velocity:${details.velocity}" ),
-        onHorizontalDragStart: (details) => setState(() => _messageOnHorizontalDragStart = "globalPosition:${details.globalPosition}" ),
-        onHorizontalDragUpdate: (details) => setState(() => _messageOnHorizontalDragUpdate = "globalPosition:${details.globalPosition}\ndelta:${details.delta}" ),
-        onHorizontalDragEnd: (details) => setState(() => _messageOnHorizontalDragEnd = "velocity:${details.velocity}" ),
-        child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 1.0,
-            heightFactor: 0.6,
-            child: Container(
-              color: Colors.green,
-              child: Center(
-                child: Column(
-                  children: [
-                    Text("onVerticalDragStart\n${_messageOnVerticalDragStart}"),
-                    Text("onVerticalDragUpdate\n${_messageOnVerticalDragUpdate}"),
-                    Text("onVerticalDragEnd\n${_messageOnVerticalDragEnd}"),
-                    Text("onHorizontalDragStart\n${_messageOnHorizontalDragStart}"),
-                    Text("onHorizontalDragUpdate\n${_messageOnHorizontalDragUpdate}"),
-                    Text("onHorizontalDragEnd\n${_messageOnHorizontalDragEnd}"),
-                  ],
-                )
+      body: Stack(
+        children: [
+          GestureDetector(
+            onVerticalDragStart: (details) => setState(() => _messageOnVerticalDragStart = "globalPosition:${details.globalPosition}" ),
+            onVerticalDragUpdate: (details) => setState(() => _messageOnVerticalDragUpdate = "globalPosition:${details.globalPosition}\ndelta:${details.delta}" ),
+            onVerticalDragEnd: (details) => setState(() => _messageOnVerticalDragEnd = "velocity:${details.velocity}" ),
+            onHorizontalDragStart: (details) => setState(() => _messageOnHorizontalDragStart = "globalPosition:${details.globalPosition}" ),
+            onHorizontalDragUpdate: (details) => setState(() => _messageOnHorizontalDragUpdate = "globalPosition:${details.globalPosition}\ndelta:${details.delta}" ),
+            onHorizontalDragEnd: (details) => setState(() => _messageOnHorizontalDragEnd = "velocity:${details.velocity}" ),
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 1.0,
+                heightFactor: 0.6,
+                child: Container(
+                  color: Colors.green,
+                  child: Center(
+                      child: Column(
+                        children: [
+                          Text("onVerticalDragStart\n${_messageOnVerticalDragStart}"),
+                          Text("onVerticalDragUpdate\n${_messageOnVerticalDragUpdate}"),
+                          Text("onVerticalDragEnd\n${_messageOnVerticalDragEnd}"),
+                          Text("onHorizontalDragStart\n${_messageOnHorizontalDragStart}"),
+                          Text("onHorizontalDragUpdate\n${_messageOnHorizontalDragUpdate}"),
+                          Text("onHorizontalDragEnd\n${_messageOnHorizontalDragEnd}"),
+                        ],
+                      )
+                  ),
+                ),
               ),
-            ),
+            )
           ),
-        )
-      ),
+          Positioned(
+            left: 20,
+            top: 20,
+            width: 10,
+            height: 10,
+            child: Container(color: Colors.blue),
+          ),
+          Positioned(
+            left: 380,
+            top: 730,
+            width: 10,
+            height: 10,
+            child: Container(color: Colors.red),
+          ),
+        ],
+      )
     );
   }
 }
