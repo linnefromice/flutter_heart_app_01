6c25c74ea17b7f47071d6946e54526520f8bc65a
linnefromice <linnefromice@gmail.com>
2020-11-12 01:00:43 +0900

    update screen - use pan & scale

--- lib/screens/gesture_detector_screen_two.dart
+++ lib/screens/gesture_detector_screen_two.dart
@@ -6,13 +6,68 @@ class GestureDetectorScreenTwo extends StatefulWidget {
 }
 
 class _State extends State<GestureDetectorScreenTwo> {
+  Offset _offset = Offset(10,10);
+  double _radians = 0.0;
+  double _scale = 1.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('GestureDetectorTwo'),
       ),
-      body: Container()
+      body: Stack(
+        children: <Widget>[
+          // Panテスト用のウィジェット
+          Positioned(
+            left: _offset.dx, // 移動の値（x）
+            top: _offset.dy, // 移動の値（y）
+            child: GestureDetector(
+              // ドラッグの移動を更新
+              onPanUpdate: (DragUpdateDetails details) {
+                setState(() {
+                  _offset = Offset(_offset.dx+details.delta.dx, _offset.dy+details.delta.dy);
+                });
+              },
+              child: Container(
+                color: Colors.green,
+                width: 100,
+                height: 100,
+                child: Center(child:Text('Pan\nx:${_offset.dx.toInt()}\ny:${_offset.dy.toInt()}'),),
+              ),
+            ),
+          ),
+          // Scale用のウィジェット
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  // 回転とスケールの値を更新
+                  onScaleUpdate: (ScaleUpdateDetails details) {
+                    setState(() {
+                      _radians = details.rotation;
+                      _scale = details.scale;
+                    });
+                  },
+                  child: Transform.rotate(
+                    angle: _radians, // 回転の値
+                    child: Transform.scale(
+                      scale: _scale, // スケールの値
+                      child: Container(
+                        height: 300,
+                        width: 300,
+                        color: Colors.amber,
+                        child: Center(child:Text('rotation:\n$_radians\nscale:\n$_scale'),),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
