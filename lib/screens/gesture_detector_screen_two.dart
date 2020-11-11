62f89521e95b7193868743473b5a21da92801f01
linnefromice <linnefromice@gmail.com>
2020-11-12 00:58:15 +0900

    create new screen

---
+++ lib/screens/gesture_detector_screen_two.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GestureDetectorScreenTwo extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<GestureDetectorScreenTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GestureDetectorTwo'),
+      ),
+      body: Container()
+    );
+  }
+}
